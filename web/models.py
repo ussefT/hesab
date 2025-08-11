ef0bb5cfe91a1fe0073d6cc76f42f1b05ac93881
usef <usef.tootian@gmail.com>
2025-08-11 07:34:15 -0700

    Add model incoming

--- web/models.py
+++ web/models.py
@@ -9,4 +9,8 @@ class Expense(models.Model):
     amount=models.BigIntegerField()
     user=models.ForeignKey(User,on_delete=models.CASCADE)
 
-
+class Income(models.Model):
+    text=models.CharField(max_length=255)
+    date=models.DateTimeField()
+    amount=models.BigIntegerField()
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
